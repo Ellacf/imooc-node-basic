42f4e23a5b14ce419e8e2d05c0df215fb88ddfd5
Ella <frabbit_cf@163.com>
2016-04-29 16:59:11 +0800

    基本测试页面

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+app.listen(port);
+
+console.log('imooc started on port ' + port);
+
+// index page
+app.get('/',function(req, res){
+    res.render('index', {
+        title: 'imooc 首页'
+    });
+});
+
+// detail page
+app.get('/movie/:id',function(req, res){
+    res.render('detail', {
+        title: 'imooc 详情页'
+    });
+});
+
+// admin page
+app.get('/admin/movie',function(req, res){
+    res.render('admin', {
+        title: 'imooc 后台录入页'
+    });
+});
+
+// list page
+app.get('/admin/list',function(req, res){
+    res.render('list', {
+        title: 'imooc 列表页'
+    });
+});
